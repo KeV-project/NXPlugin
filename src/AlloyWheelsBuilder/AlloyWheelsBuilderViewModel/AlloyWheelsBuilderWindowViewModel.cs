520ec585e3189d18f81e73a8112afad654537b6c
• Katovskaya <katovskaya009@gmail.com>
2021-10-28 15:52:15 +0700

    feature(AlloyWheelsBuilder): добавлен проект AlloyWheelsBuilderModel

--- src/AlloyWheelsBuilder/AlloyWheelsBuilderViewModel/AlloyWheelsBuilderWindowViewModel.cs
+++ src/AlloyWheelsBuilder/AlloyWheelsBuilderViewModel/AlloyWheelsBuilderWindowViewModel.cs
@@ -4,11 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace AlloyWheelsBuilderViewModel
 {
     public class AlloyWheelsBuilderViewModel: ViewModelBase
     {
-        
+        public AlloyWheelsBuilderViewModel()
+		{
+
+		}
+
+        private RelayCommand _cancelCommand;
+
+        public RelayCommand CancelCommand
+		{
+			get
+			{
+                return _cancelCommand ??
+                  (_cancelCommand = new RelayCommand(() =>
+                  {
+                      
+                  }));
+            }
+		}
+
+        private RelayCommand _buildCommand;
+
+        public RelayCommand BuildCommand
+		{
+			get
+			{
+                return _buildCommand ??
+                  (_buildCommand = new RelayCommand(() =>
+                  {
+
+                  }));
+            }
+		}
     }
 }

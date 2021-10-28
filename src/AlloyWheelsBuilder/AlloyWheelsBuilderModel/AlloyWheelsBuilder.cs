c763aa8d5a36d2c132be45a8c61063216b2d2e07
• Katovskaya <katovskaya009@gmail.com>
2021-10-28 16:12:24 +0700

    feature(AlloyWheelsBuilderModel): создан класс ArcData

--- src/AlloyWheelsBuilder/AlloyWheelsBuilderModel/AlloyWheelsBuilder.cs
+++ src/AlloyWheelsBuilder/AlloyWheelsBuilderModel/AlloyWheelsBuilder.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NXOpen;
 
 namespace AlloyWheelsBuilderModel
 {
     public static class AlloyWheelsBuilder
     {
+        private static Session _session;
+
+        private static Part _workPart;
+
         public static void Build(AlloyWheelsData alloyWheelsData)
 		{
+            _session = Session.GetSession();
 
-		}
+            _workPart = _session.Parts.Work;
+        }
     }
 }
---
+++ src/AlloyWheelsBuilder/AlloyWheelsBuilderModel/ArcData.cs
@@ -0,0 +1,20 @@
+﻿using NXOpen;
+
+namespace AlloyWheelsBuilderModel
+{
+	public class ArcData
+	{
+		public Point3d StartPoint { get; set; }
+
+		public Point3d PointOn { get; set; }
+
+		public Point3d EndPoint { get; set; }
+
+		public ArcData(Point3d startPoint, Point3d pointOn, Point3d endPoint)
+		{
+			StartPoint = startPoint;
+			PointOn = pointOn;
+			EndPoint = endPoint;
+		}
+	}
+}

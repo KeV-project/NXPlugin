7983fd36686ee6e817f1897736e68154d9040b80
• Katovskaya <katovskaya009@gmail.com>
2021-12-01 01:19:43 +0700

    docs(StartAlloyWheelsBuilder): обавлены xml-комментарии

--- src/AlloyWheelsBuilder/AlloyWheelsBuilder/StartAlloyWheelsBuilder.cs
+++ src/AlloyWheelsBuilder/AlloyWheelsBuilder/StartAlloyWheelsBuilder.cs
@@ -2,14 +2,26 @@
 
 namespace AlloyWheelsBuilder
 {
+    /// <summary>
+    /// Класс <see cref="StartAlloyWheelsBuilder"/> предназначен для 
+    /// выполнения запуска окна для ввода параметров модели
+    /// </summary>
     public class StartAlloyWheelsBuilder
     {
+        /// <summary>
+        /// Вызывает окно для ввода параметров модели
+        /// </summary>
+        /// <param name="args">Внешние параметры</param>
         public static void Main(string[] args)
         {
             AlloyWheelsBuilderWindow mainWindow = new AlloyWheelsBuilderWindow();
             mainWindow.ShowDialog();
         }
 
+        /// <summary>
+        /// Завершает процесс NX
+        /// </summary>
+        /// <returns></returns>
         public static int GetUnloadOption(string dummy)
         {
             return (int)Session.LibraryUnloadOption.Immediately;

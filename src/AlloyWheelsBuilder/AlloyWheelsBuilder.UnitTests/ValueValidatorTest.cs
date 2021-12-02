58bbe4ece83bc6eb82ec966dd129ab1f6d483f57
• Katovskaya <katovskaya009@gmail.com>
2021-12-02 22:23:21 +0700

    feature(ValueValidatorTest): добавлены тесты метода AssertNumberInRange

---
+++ src/AlloyWheelsBuilder/AlloyWheelsBuilder.UnitTests/ValueValidatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using AlloyWheelsBuilderModel;
+
+namespace AlloyWheelsBuilder.UnitTests
+{
+	[TestFixture]
+	public class ValueValidatorTest
+	{
+		[Test(Description = "Позитивный тест метода AssertNumberInRange")]
+		public void TestAssertNumberInRange_СorrectValue()
+		{
+			// setup
+			const double diameter = 440;
+			const double minDiameter = 101.6;
+			const double maxDiameter = 1117.2;
+			const string context = "диаметр диска";
+
+			// act
+			ValueValidator.AssertNumberInRange(diameter, minDiameter,
+			  maxDiameter, context);
+		}
+
+		[Test(Description = "Негативный тест метода AssertNumberInRange")]
+		public void TestAssertNumberInRange_IncorrectValue()
+		{
+			// setup
+			const double wrongDiameter = 440;
+			const double minDiameter = 666.6;
+			const double maxDiameter = 1117.2;
+			const string context = "диаметр диска";
+
+			// assert
+			Assert.Throws<ArgumentException>(() =>
+			{
+				ValueValidator.AssertNumberInRange(wrongDiameter, minDiameter,
+					maxDiameter, context);
+			}, "Должно возникать искючение, " +
+			"если проверяемое число не входит в допустимый диапазон");
+		}
+	}
+}

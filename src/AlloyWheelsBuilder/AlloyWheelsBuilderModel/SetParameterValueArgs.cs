1909a402435842abf087c61f9e1aea82a510d821
• Katovskaya <katovskaya009@gmail.com>
2021-12-28 19:17:48 +0700

    docs(SetParameterValueArgs): добавлены xml-комментарии

--- src/AlloyWheelsBuilder/AlloyWheelsBuilderModel/SetParameterValueArgs.cs
+++ src/AlloyWheelsBuilder/AlloyWheelsBuilderModel/SetParameterValueArgs.cs
@@ -2,13 +2,30 @@
 
 namespace AlloyWheelsBuilderModel
 {
-    //TODO: XML
+    //TODO: XML +
+
+    /// <summary>
+    /// Класс <see cref="SetParameterValueArgs"/> 
+    /// представляет информацию о параметре, 
+    /// который не прошел валидацию
+    /// </summary>
     public class SetParameterValueArgs: EventArgs
     {
+        /// <summary>
+        /// Возвращает и устанавливает имя параметра
+        /// </summary>
         public AlloyWheelsParameterName ParameterName { get; set; }
 
+        /// <summary>
+        /// Возвращает и устанавливает сообщение об ошибке
+        /// </summary>
         public string Message { get; set; }
 
+        /// <summary>
+        /// Инициализирует объект класса <see cref="SetParameterValueArgs"/>
+        /// </summary>
+        /// <param name="parameterName">Имя параметра</param>
+        /// <param name="message">Сообщение об ошибке</param>
         public SetParameterValueArgs(AlloyWheelsParameterName parameterName,
             string message)
 		{
